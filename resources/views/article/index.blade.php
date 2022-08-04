344969ab2208ece3343737897a3551edb4193cb9
ghaith <a89ghaith@gmail.com>
2022-08-04 09:32:35 +0300

    fix time

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -4,13 +4,16 @@
         <h1>Articles</h1>
         <a class="btn btn-success" href="{{ route('article.create') }}">Create New Article</a>
     </div>
-    @foreach ($articles as $article )
+    @foreach ($articles as $article)
         <div class="card my-3">
             <div class="card-body">
                 <h5 class="card-title">{{ $article->title }}</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                    content.
                     This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">{{ $article->created_at->diffForHumans() }}</small></p>
+                <p class="card-text">
+                    <small class="text-muted">{{ $article->created_at->diffForHumans() }}</small>
+                </p>
             </div>
         </div>
     @endforeach
